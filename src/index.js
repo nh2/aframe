cd93db0ad20acb1774b61494f03b23d377c2df28
Francesco Mazzoli <f@mazzo.li>
2018-04-21 10:42:29 +0200

    do not print versioning info unless debug is enabled

--- src/index.js
+++ src/index.js
@@ -83,9 +83,11 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 0.8.0 (Date 2018-04-21, Commit #34da9b2)');
-console.log('three Version:', pkg.dependencies['three']);
-console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
+if (window.anapoDebug) {
+  console.log('A-Frame Version: 0.8.0 (Date 2018-04-21, Commit #34da9b2)');
+  console.log('three Version:', pkg.dependencies['three']);
+  console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
+}
 
 module.exports = window.AFRAME = {
   AComponent: require('./core/component').Component,
